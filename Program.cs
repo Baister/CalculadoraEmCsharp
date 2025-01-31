32aa13ff320db3b3699d82368e2ace4dbaa51f5d
Baister <gustavobaister@gmail.com>
2025-01-31 12:01:39 -0300

    Adicionando Saída ao programa

--- Program.cs
+++ Program.cs
@@ -6,21 +6,33 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Deseja a operação que deseja realizar 1 ou 2: ");
-            int escolha = Convert.ToInt32(Console.ReadLine());
-            switch (escolha)
-            {
-                case 1:
-                    Soma();
-                    break;
-                case 2:
-                    Subtrair();
-                    break;
-                default:
-                    Console.WriteLine("Decida algo.");
-                    break;
-            }
+            Menu();
+        }
 
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("---------");
+            Console.WriteLine("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine()); //Menor número que podemos ter
+
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+
+                default: Menu(); break;
+            }
         }
 
         static void Soma()
@@ -36,9 +48,12 @@ namespace Calculator
 
             Console.WriteLine("");
             Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ReadKey();
+
+            Menu(); //Para trazer a tela inicial de volta
         }
 
-        static void Subtrair()
+        static void Subtracao()
         {
             Console.Clear();
             Console.Write("Primeiro valor: ");
@@ -51,6 +66,48 @@ namespace Calculator
 
             Console.WriteLine("");
             Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+
+            Menu(); //Para trazer a tela inicial de volta
+
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 / v2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+
+            Menu(); //Para trazer a tela inicial de volta
+
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+
+            Menu(); //Para trazer a tela inicial de volta
+
         }
     }
 }
